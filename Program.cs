7d49d09a2d74bb9fdbb797cfdaf3d661c901d985
jhcsharp01 <makebaram@gmail.com>
2025-02-03 15:53:19 +0900

    함수, 변수 범위

--- Program.cs
+++ Program.cs
@@ -1,23 +1,15 @@
-﻿namespace L20250203
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace L20250203
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static int[,] data = new int[10, 10];
+
+        static void Initialze() //PascalCase
         {
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-
-            int[,] data = new int[10, 10];
-
             int number = 1;
+
             for (int j = 0; j < 10; j++)
             {
                 for (int i = 0; i < 10; i++)
@@ -25,15 +17,24 @@
                     data[j, i] = number++;
                 }
             }
+        }
 
+        static void Print()
+        {
             for (int j = 0; j < 10; j++)
             {
                 for (int i = 0; i < 10; i++)
                 {
-                    Console.Write(data[j, i].ToString() + "\t" );
+                    Console.Write(data[j, i].ToString() + "\t");
                 }
                 Console.WriteLine();
             }
+        }
+
+        static void Main(string[] args)
+        {
+            Initialze();
+            Print();
         }
     }
 }
